d0698c5dd5bd5b2c85abca1db9e4a06845d74247
chenchaofan <ChenChaoFan90@163.com>
2020-05-27 16:17:40 +0800

    初始化工程

---
+++ cloud-provider-payment8001/src/main/java/com/springcloud/dao/PaymentDao.java
@@ -0,0 +1,16 @@
+package com.springcloud.dao;
+
+import com.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+
+/**
+ * @author chenchaofan
+ */
+@Mapper
+public interface PaymentDao {
+     int create(Payment payment);
+
+     Payment getPaymentById(@Param("id") Long id);
+}
